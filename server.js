4e81ccfff15879196c8007b742ceea38b39a982c
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-05-19 22:01:13 -0500

    Added logger middleware.

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.set('view engine', 'hbs');
 
 app.use(express.static(`${__dirname}/public`));
 
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  console.log(`${now}: ${req.method} ${req.url}`);
+  next();
+});
+
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 
 hbs.registerHelper('screamIt', (text) => text.toUpperCase());
